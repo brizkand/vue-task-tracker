453d2d9e0a819a38c71eeba4ad0a9a78679eba63
brizkand <holgadokevin@gmail.com>
2023-02-10 00:07:51 +0800

    This commit is to make a branch for static code of task tracker

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button @btn-click="$emit('toggle-add-task')" :text="showAddTaskButton ? 'Close' : 'Add Task'" :color="showAddTaskButton ? 'red' : 'green'"/>
+  </header>
+</template>
+
+<script>
+  import Button from './Button';
+
+  export default {
+    name: 'Header',
+    props: {
+      title: String,
+      showAddTaskButton: Boolean
+    },
+    components: {
+      Button,
+    }
+  }
+</script>
+
+<style scoped> 
+  header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+  }
+</style>
